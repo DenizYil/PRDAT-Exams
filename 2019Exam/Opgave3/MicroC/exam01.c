f5c007095976d218ee753061b9b9e4a283eb9733
DenizYil <denizyil@outlook.dk>
2023-01-08 19:42:14 +0100

    t

--- 2019Exam/Opgave3/MicroC/exam01.c
+++ 2019Exam/Opgave3/MicroC/exam01.c
@@ -1,11 +1,21 @@
-void main(int n) {
-
-    // x = user input
-
-    while (n > 0) {
-        print n;
-        break false;
-        n = n - 1;
+void printArray (int a[]) {
+    int i;
+    i=0;
+    /* Describe stack content at this point. */
+    while (i<5) {
+        print (a[i]);
+        i=i+1;
     }
-    println;
+}
+
+void main() {
+    int a[5];
+    int i;
+    i=0;
+    while (i<5) {
+        a[i] = 42;
+        i=i+1;
+    }
+    printArray(a);
+    print 43; /* Statement to be removed in question 2. */
 }
